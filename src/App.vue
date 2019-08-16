13d477de28eefa0a8d71565df6daf03ded6fa22b
Mike Langone <langone@Mikes-MacBook-Pro.local>
2019-08-16 09:41:55 -0600

    changes to send to jdog

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="app">
-    <!-- access root props via $root -->
 
-<div id="langoneContainer">
+    <div id="vueContainer">
 
+        <image-swap></image-swap>
 
-    <image-swap></image-swap>
-    <div>
-      <a href="https://certapro.com/" target="_blank" rel="noopener">
-      <img id="logo-certapro" src="https://ptopads.com/wp-content/themes/oi-mastertheme-lean/assets/images/svg/certapro_logo.svg" alt="CertaPro logo" align="center" class="float-center" />
-      </a>
     </div>
-    <div>
-        <a href="https://floorcoveringsinternational.com/" target="_blank" rel="noopener">
-            <img id="logo-fci" src="https://ptopads.com/wp-content/uploads/2019/05/fci-logo.png" alt="Floor Coverings International logo" align="center" class="float-center" />
-        </a>
-    </div>
-    <div>
-        <a href="https://www.adt.com/" target="_blank" rel="noopener">
-            <img id="logo-adt" src="https://ptopads.com/wp-content/themes/oi-mastertheme-lean/assets/images/svg/adt-new-logo.svg" alt="ADT logo" align="center" class="float-center" />
-        </a>
-    </div>
-</div>
-    <router-view />
+    
   </div>
 </template>
 
